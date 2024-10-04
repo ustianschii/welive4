50631ab22539a335348221a15d0d72d533732fbe
Cristian <ustianschii@gmail.com>
2024-10-04 12:56:57 +0300

    analytics component fixed

--- components/analytics/analytics.tsx
+++ components/analytics/analytics.tsx
@@ -5,11 +5,11 @@ export const Analytics = () => {
   return (
     <>
       <Script
-        strategy="lazyOnload"
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
       />
 
-      <Script id="" strategy="lazyOnload">
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
