3f8bd83072565b9c58535454df5e31056352c4cd
Cristian <ustianschii@gmail.com>
2024-11-16 14:19:18 +0200

    test commit

--- components/hero-title/index.tsx
+++ components/hero-title/index.tsx
@@ -4,7 +4,7 @@ import { HeaderTextBox, Title, Highlighted, Subtitle } from "./styles";
 export const HeroTitle = () => {
   return (
     <HeaderTextBox>
-      <Title>EXPERIENCE SMART TECHNOLOGY TODAY</Title>
+      <Title>EXPERIENCE SMART TECHNOLOGY TODAY!</Title>
       <Subtitle>
         WE
         <Highlighted>LIVE</Highlighted>4 THE
